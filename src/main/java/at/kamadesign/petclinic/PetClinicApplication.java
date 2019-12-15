4b8d9e9057c9843e49f5fe0ec4226e0e9dfee069
Mathias Schaller <stoertebecker@gmx.at>
2019-12-15 14:23:59 +0100

    Start

---
+++ src/main/java/at/kamadesign/petclinic/PetClinicApplication.java
@@ -0,0 +1,13 @@
+package at.kamadesign.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicApplication.class, args);
+    }
+
+}
